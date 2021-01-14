4c3c9915e7492c04131e7693325f403472453f06
Alessandro Vozza <alessandro.vozza@microsoft.com>
2021-01-15 00:59:21 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+export ARM_CLIENT_ID=$INPUT_ARM_CLIENT_ID
+export ARM_CLIENT_SECRET=$INPUT_ARM_CLIENT_SECRET
+export ARM_SUBSCRIPTION_ID=$INPUT_ARM_SUBSCRIPTION_ID
+export ARM_TENANT_ID=$INPUT_ARM_TENANT_ID
+export VARIABLES=$INPUT_VARIABLES
+export INPUT_PATH=$INPUT_PATH
+
+cd /github/workspace/$INPUT_PATH
+
+echo "terraform init -input=false"
+terraform init -input=false
+
+var_args=""
+VARIABLES=$(echo "$VARIABLES" | tr "," "\n")
+for var in $VARIABLES; do
+  var_args="$var_args -var $var"
+done
+
+echo "Terraform init"
+terraform init -no-color
+
+echo "Terraform plan"
+terraform plan -no-color
+
+echo "Applying Terraform templates"
+terraform apply -no-color -input=false -auto-approve $var_args
+
+echo "Applying Terraform templates"
+terraform destroy -no-color -input=false -auto-approve
