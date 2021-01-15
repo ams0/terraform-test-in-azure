1ec2a57c9dd54d7be7533ef9aa98f6594d1d2c30
Alessandro Vozza <alessandro.vozza@microsoft.com>
2021-01-15 01:39:31 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,24 +11,14 @@ export INPUT_PATH=$INPUT_PATH
 
 cd /github/workspace/$INPUT_PATH
 
-
-var_args=""
-VARIABLES=$(echo "$VARIABLES" | tr "," "\n")
-for var in $VARIABLES; do
-  var_args="$var_args -var $var"
-done
-
-echo "var_args:"
-echo $var_args
-
 echo "Terraform init"
 terraform init -no-color
 
 echo "Terraform plan"
-terraform plan -no-color
+terraform plan -no-color $VARIABLES
 
 echo "Applying Terraform templates"
-terraform apply -no-color -input=false -auto-approve $var_args
+terraform apply -no-color -input=false -auto-approve $VARIABLES
 
 echo "Destroying infrastructure"
 terraform destroy -no-color -input=false -auto-approve
